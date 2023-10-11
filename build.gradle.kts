4213bddc5e0f57efb095a8aa67a276e66962e042
object-Object <object@objectobject.ca>
2023-10-11 19:57:14 -0400

    publishn't

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,6 @@ plugins {
 
     id("fabric-loom") version "0.12.+"
     id("io.github.juuxel.loom-quiltflower") version "1.7.+"
-
-    id("com.modrinth.minotaur") version "2.+"
-    id("me.hypherionmc.cursegradle") version "2.+"
-    id("com.github.breadmoirai.github-release") version "2.+"
-    id("io.github.p03w.machete") version "1.+"
-    `maven-publish`
 }
 
 group = "dev.isxander"
@@ -99,94 +93,5 @@ tasks {
         dependsOn("curseforge")
         dependsOn("publish")
         dependsOn("githubRelease")
-    }
-}
-
-val changelogText = file("changelogs/${project.version}.md").takeIf { it.exists() }?.readText() ?: "No changelog provided"
-
-val modrinthId: String by project
-if (modrinthId.isNotEmpty()) {
-    modrinth {
-        token.set(findProperty("modrinth.token")?.toString())
-        projectId.set(modrinthId)
-        versionNumber.set("${project.version}")
-        versionType.set("release")
-        uploadFile.set(tasks["remapJar"])
-        gameVersions.set(listOf(minecraftVersion))
-        loaders.set(listOf("fabric", "quilt"))
-        changelog.set(changelogText)
-        syncBodyFrom.set(file("README.md").readText())
-
-        dependencies {
-            required.project("fabric-language-kotlin")
-            required.project("cloth-config")
-            optional.project("modmenu")
-        }
-    }
-}
-
-val curseforgeId: String by project
-if (hasProperty("curseforge.token") && curseforgeId.isNotEmpty()) {
-    curseforge {
-        apiKey = findProperty("curseforge.token")
-        project(closureOf<me.hypherionmc.cursegradle.CurseProject> {
-            mainArtifact(tasks["remapJar"], closureOf<me.hypherionmc.cursegradle.CurseArtifact> {
-                displayName = "${project.version}"
-            })
-
-            id = curseforgeId
-            releaseType = "release"
-            addGameVersion(minecraftVersion)
-            addGameVersion("Fabric")
-            addGameVersion("Java 17")
-
-            changelog = changelogText
-            changelogType = "markdown"
-
-            relations(closureOf<me.hypherionmc.cursegradle.CurseRelation> {
-                requiredDependency("fabric-language-kotlin")
-                requiredDependency("cloth-config")
-                optionalDependency("modmenu")
-            })
-        })
-
-        options(closureOf<me.hypherionmc.cursegradle.Options> {
-            forgeGradleIntegration = false
-        })
-    }
-}
-
-githubRelease {
-    token(findProperty("github.token")?.toString())
-
-    val githubProject: String by project
-    val split = githubProject.split("/")
-    owner(split[0])
-    repo(split[1])
-    tagName("${project.version}")
-    targetCommitish("1.19")
-    body(changelogText)
-    releaseAssets(tasks["remapJar"].outputs.files)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mod") {
-            groupId = "dev.isxander"
-            artifactId = "pronounify"
-
-            from(components["java"])
-        }
-    }
-
-    repositories {
-        if (hasProperty("xander-repo.username") && hasProperty("xander-repo.token")) {
-            maven(url = "https://maven.isxander.dev/releases") {
-                credentials {
-                    username = property("xander-repo.username")?.toString()
-                    password = property("xander-repo.token")?.toString()
-                }
-            }
-        }
     }
 }
